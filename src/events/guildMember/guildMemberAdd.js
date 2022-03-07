2fec8938ea391ce509890f1da170cfa3cefcc5af
gessic22 <79780250+gessic22@users.noreply.github.com>
2022-03-07 02:18:40 -0300

    mensagem de boas vindas

--- src/events/guildMember/guildMemberAdd.js
+++ src/events/guildMember/guildMemberAdd.js
@@ -1,4 +1,5 @@
 const Event = require ('../../structures/Event')
+const { MessageEmbed } = require('discord.js')
 
 module.exports = class extends Event {
   constructor(client) {
@@ -12,8 +13,21 @@ module.exports = class extends Event {
 
     if (guildDB?.welcome) {
       const welcomeChannel = member.guild.channels.cache.get(guildDB.welcome.channel)
+      
+      const embed = new MessageEmbed()
+      .setColor(0x283593)
+      .setThumbnail("https://storage.googleapis.com/img-muquest/imgs/discord/iconlogo.png")
+      .setTitle("Bem vindo a nossa comunidade Quest!")
+      .setDescription(`É muito bom que esteja aqui ${member.toString()}.\nEsse canal foi criado para que mais pessoas possam interagir e se divertir juntas, jogando um clássico dos jogos online MU, em uma plataforma solida livre de ADMs Abusers.\nTrabalhamos para que nossos jogadores possam ter a melhor experiência possível dentro de nossas plataformas, como jogadores nós da <@&947560005979238411> sabemos o quanto é importante uma plataforma funcional e bem estruturada. E que não é somente o jogo que faz algo ser divertido, as pessoas que jogam também são importantes, acarretando amizades para a vida toda.\n\n Caso não conheça acesse nosso site [muonline.quest](https://www.muonline.quest/)`)
+      .addFields(
+        { name: "Apresente-se", value: "Nada melhor do que conhecer pessoas novas <#949714310370840656>", inline: true },
+        { name: "Fique por dentro", value: "Acesse nosso canal de <#949744334545235978> e conheça as novidades.", inline: true }
+      )
+      .setImage("https://storage.googleapis.com/img-muquest/imgs/discord/pesonagens%20mu.gif")
+      .setFooter({text: "Não somos um servidor comum, somos a comunidade Quest.", iconURL: "https://storage.googleapis.com/img-muquest/imgs/discord/iconlogo.png"})
 
-      welcomeChannel?.send(`${member.toString()}, Seja bem vindo a comunidade Quest!`)
+      welcomeChannel?.send({ embeds: [embed] })
+
     }
   }
 }
--- src/structures/Client.js
+++ src/structures/Client.js
@@ -6,6 +6,7 @@ const { join } = require('path')
 const mongoose = require('mongoose')
 const Models = require('../database/models/Models')
 
+
 module.exports = class extends Client {
   constructor(options) {
     super(options)
@@ -13,6 +14,7 @@ module.exports = class extends Client {
     this.commands = []
     this.loadComands()
     this.loadEvents()
+    
   }
 
   registryCommands() {
